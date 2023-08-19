cbcf5afc9c29d47f0706426f4dd4f703c608a4c9
dreaMykj <gabrielgreghi2018@gmail.com>
2023-08-19 13:33:12 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string nome, nome2, help;
+
+Console.WriteLine("Troca de Valores");
+
+Console.Write("Nome---");
+nome = Console.ReadLine()!;
+
+Console.Write("Nome2---");
+nome2 = Console.ReadLine()!;
+
+help = nome;
+nome = nome2;
+nome2 = help;
+
+
+Console.WriteLine("Trocando os valores");
+Console.WriteLine($"nome = {nome}");
+Console.WriteLine($"nome2 = {nome2}");
